e92ebc4de3d8da3ccf62c87c9f14cce78d4792c8
CloverEmilia <141259504+CloverEmilia@users.noreply.github.com>
2023-08-05 08:03:27 -0400

    no

--- Chess-Challenge/src/Framework/Application/Helpers/SetDirectoriesChecker.cs
+++ Chess-Challenge/src/Framework/Application/Helpers/SetDirectoriesChecker.cs
@@ -4,7 +4,7 @@ namespace ChessChallenge.Application
 {
     public static class DirectoriesSetLocation
     {
-        public const string stockFishDirectory = "//put the full (including C:/) directory of stockfish here";
+        public const string stockFishDirectory = @"//put the full (including C:/) directory of stockfish here";
         public const bool isStockFishDirectoryDefault = (stockFishDirectory == "//put the full (from C:/) directory of stockfish here") ? true : false;
     }
 }
