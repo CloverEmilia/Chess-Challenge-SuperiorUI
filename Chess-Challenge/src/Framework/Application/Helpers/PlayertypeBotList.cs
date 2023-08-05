ac968fc3d022a509bc3eabc59f0ca795e0dad9eb
CloverEmilia <141259504+CloverEmilia@users.noreply.github.com>
2023-08-05 07:32:06 -0400

    Will probably drop, but need to commit so I can discard changes

---
+++ Chess-Challenge/src/Framework/Application/Helpers/PlayertypeBotList.cs
@@ -0,0 +1,34 @@
+﻿using ChessChallenge.API;
+using System.Collections.Generic;
+using System;
+
+
+namespace ChessChallenge.Application
+{
+    
+    public static class PlayertypeBotList
+    {
+        //As far as I know
+        //There's no legitimate reason whatsoever to manually edit this file
+        //but you do you
+
+        public enum BotPlayerType
+        {
+            //Begin ListA
+            Human,
+            MyBot,
+            EvilBot,
+            //ect.
+        }
+
+        public static readonly Dictionary<PlayerType, Type> BotTypeMap = new Dictionary<PlayerType, Type>
+        {
+            //Humans aren't real
+            //Begin ListB
+            { PlayerType.MyBot, typeof(MyBot) },
+            { PlayerType.EvilBot, typeof(EvilBot) },
+            //ect.
+        };
+
+    }
+}
